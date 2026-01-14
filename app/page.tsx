9158d7aed06c05996a24bd62fe8dce3a01e1f5b4
Nova <aelithron@gmail.com>
2026-01-13 21:48:32 -0700

    start building the form on the front page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8 md:p-16">
       <h1 className="font-semibold text-4xl">Widgets</h1>
-      <p className="text-lg italic">Create aesthetic widgets for your websites <FontAwesomeIcon icon={faHeart} /></p>
+      <p className="text-lg italic">Create beautiful widgets for your websites <FontAwesomeIcon icon={faHeart} /></p>
       <WidgetForm />
     </main>
   );
--- app/widget.form.tsx
+++ app/widget.form.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Widget } from "@/widgets";
 import { faTableCellsLarge } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
@@ -6,11 +7,23 @@ import { useState } from "react";
 type EditorPage = "base" | "";
 export function WidgetForm() {
   const [page, setPage] = useState<EditorPage>("base");
+  const [type, setType] = useState<Widget>("time")
   return (
     <div className="flex flex-col gap-2 mt-4">
       <div className="flex gap-2 bg-slate-500 rounded-xl p-2">
         <button onClick={() => setPage("base")} className={`hover:text-sky-500 p-1 ${page === "base" && "bg-slate-600 rounded-xl"}`}><FontAwesomeIcon icon={faTableCellsLarge} /></button>
       </div>
+      <form className="mt-2 rounded-xl p-2 flex bg-slate-400 dark:bg-slate-700">
+        {page === "base" && <div className="grid grid-cols-3 gap-4 items-center">
+          <div className="flex flex-col gap-2">
+            <label className="text-sm font-semibold" htmlFor="type">Widget Type</label>
+            <select className="bg-slate-500 rounded-lg p-1" id="type" value={type} onChange={(e) => setType(e.target.value as Widget)}>
+              <option value="time">Time</option>
+              <option value="weather">Weather</option>
+            </select>
+          </div>
+        </div>}
+      </form>
     </div>
   );
 }
